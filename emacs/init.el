668a580a8ff8f446c01642bb807fbcf425604983
Andrew Tulloch <andrew@tullo.ch>
2014-04-07 00:22:05 +0100

    Updated

--- emacs/init.el
+++ emacs/init.el
@@ -1,5 +1,11 @@
+;;; Init --- Summary
+;;; Commentary:
+;;; Code:
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 (require 'pallet)
 
 (mapc 'load (directory-files "~/.emacs.d/customizations" t "^[0-9]+.*\.el$"))
+
+(provide 'init)
+;;; init.el ends here
