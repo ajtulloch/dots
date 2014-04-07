11bb8f67985d8fc2bde8e89b85fc21b55cf352df
Andrew Tulloch <andrew@tullo.ch>
2014-04-07 10:48:41 +0100

    Cleanup of some dead code.
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:

--- emacs/customizations/02-global.el
+++ emacs/customizations/02-global.el
@@ -2,28 +2,8 @@
 ;;; Commentary:
 ;;; Code:
 
-;; (load-theme 'solarized-dark t)
-
-;; (smex-initialize)
-;; (require 'expand-region)
-;; (global-set-key (kbd "C-=") 'er/expand-region)
-;; (ido-mode 1)
-;; (ido-everywhere 1)
-;; (flx-ido-mode 1)
-;; disable ido faces to see flx highlights.
-;; (require 'ess-site)
-;; (add-hook 'jade-mode-hook (lambda ()
-;;                             (setq yas/dont-activate t)))
-;; (add-hook 'org-mode-hook
-;;           #'(lambda ()
-;;               (setq yas/fallback-behavior
-;;                     `(apply ,(lookup-key org-mode-map [tab])))
-;;               (local-set-key [tab] 'yas/expand)
-;;               (define-key yas/keymap [tab] 'yas/next-field-or-maybe-expand)))
-;; disable auto-save and auto-backup
-;; (setq auto-save-default nil)
-;; (setq make-backup-files nil)
-;; (setq ido-use-faces nil)
-
 ;; open gist page on github
 (setq gist-view-gist t)
+
+(provide '02-global)
+;;; 02-global.el ends here
