0dd02d5cb00c358cd0722c1f5fe2abb07390d09b
Andrew Tulloch <andrew@tullo.ch>
2014-04-23 15:19:13 +0100

    updates

--- emacs/customizations/01-tulloch.el
+++
@@ -1,7 +0,0 @@
-;;; tulloch --- Summary
-;;; Commentary:
-;;; Code:
-;; Global configuration
-
-(provide '01-tulloch)
-;;; 01-tulloch.el ends here
---
+++ emacs/customizations/20-js.el
@@ -0,0 +1,9 @@
+;;; js --- Summary
+;;; Commentary:
+;;; Code:
+(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(add-hook 'js2-mode-hook (lambda ()
+                           (auto-fill-mode -1)))
+
+(provide '20-js)
+;;; 20-scala.el ends here
