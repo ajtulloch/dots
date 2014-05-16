8734743e46ac168205f2bd06ff691a0b10706e60
Andrew Tulloch <andrew@tullo.ch>
2014-05-16 13:00:56 +0100

    Added OS X Settings.
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    CC:

--- osx-settings.sh
+++ osx-settings.sh
@@ -2,4 +2,4 @@ set -ex
 defaults write NSGlobalDomain KeyRepeat -int 0
 defaults write com.sublimetext.3 ApplePressAndHoldEnabled -bool false
 defaults write com.apple.iTunes show-store-arrow-links -bool FALSE
-
+# defaults write ~/.MacOSX/environment PATH "`echo $PATH`"
